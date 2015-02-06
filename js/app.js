d718319cba1fb175a52c280b64a9ccd7a987186e
Josh Wolf <josh@greenlightgo.com>
2015-02-06 17:14:25 -0500

    initial

---
+++ js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var myApp = angular.module('myApp', ['ngRoute', 'ui.bootstrap', 'myAppControllers'])
+    });
+
+myApp.config(['$routeProvider',
+    function($routeProvider) {
+    	$routeProvider
+    		.when('/', {
+    			templateUrl: '/partials/index.html',
+    			controller: 'MyAppCtrl'
+    		})
+        .otherwise({ redirectTo: '/'});
+      }
+    ]);
+
---
+++ js/controllers.js
@@ -0,0 +1,6 @@
+var myAppControllers = angular.module('myAppControllers', []);
+
+myAppControllers.controller('MyAppCtrl', ['$scope', '$http', '$window', '$modal', '$location', '$routeParams',
+  function ($scope, $http, $window, $modal, $location, $routeParams) {
+    }]);
+
